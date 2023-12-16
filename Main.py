c96ba663a594b0b24633a22650af9d1342167898
Matecitos <31082992+Kevinmoth@users.noreply.github.com>
2023-12-16 18:38:54 -0300

    Update Main.py

--- Main.py
+++ Main.py
@@ -8,7 +8,7 @@ bot = commands.Bot(command_prefix='x', intents=discord.Intents.default())
 guild_id = 765279392137871400
 channel_id = 906646235904082012
 
-frases = ["Frase 1", "Frase 2", "Frase 3", "Frase 4"]  # Agrega las frases que quieras aquí
+frases = ["Frase 1", "Frase 2", "Frase 3", "Frase 4"]  # frases o palabrasque se enviaran de manera aleatoria
 
 @bot.event
 async def on_ready():
@@ -19,7 +19,7 @@ async def on_ready():
 
 @tasks.loop(minutes=2)
 async def responder_temporizador():
-    # Obtener el canal del servidor
+    # sacamos un canal del server
     guild = bot.get_guild(guild_id)
     channel = guild.get_channel(channel_id)
 
@@ -35,10 +35,9 @@ async def on_message(message):
     if message.author.bot:
         return
 
-    # Puedes agregar aquí más lógica para responder al mensaje original si es necesario
-    # await message.channel.send("¡Hola! He visto tu mensaje.")
+    # en construccion, ia para reconocer palabras
 
-    # Asegúrate de llamar al evento original para que otros eventos también se manejen correctamente
+   
     await bot.process_commands(message)
 
 bot.run("token")
